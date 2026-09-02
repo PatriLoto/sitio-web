44b75c1274029e43a8f9f325c12ade2d80cbf528
Patricia A. Loto <39300655+PatriLoto@users.noreply.github.com>
2026-09-02 08:39:14 -0300

    Update display-options.js

--- display-options.js
+++ display-options.js
@@ -46,7 +46,8 @@
     const root    = document.documentElement;
     const body    = document.body;
     const overlay = document.getElementById('do-overlay');
-    const container = document.getElementById('quarto-document-content.content') || body;
+    //const container = document.getElementById('quarto-document-content.content') || body;
+    const container = document.querySelector('#quarto-document-content.content') || body;
 
     root.style.fontSize   = s.fontSize + 'px';
     body.style.lineHeight = (s.spacing / 100).toFixed(2);
